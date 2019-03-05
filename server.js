39937bee3c884d997ad0159d30aef70c05d0d6b5
shailendrabhargava93 <shailendrabhargavaa@gmail.com>
2019-03-05 18:00:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+app.all('*', (req, res) => {  
+  res.status(200).sendFile(__dirname + '/dist/index.html');  
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
