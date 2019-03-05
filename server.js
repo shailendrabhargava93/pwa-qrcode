70bd63dd8d27b0d3cfb1309c7a7684aa0b6a68c9
shailendrabhargava93 <shailendrabhargavaa@gmail.com>
2019-03-05 18:07:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/pwa-qrcode'));
 
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/pwa-qrcode/index.html');  
 });
 
 // Start the app by listening on the default
